66562b58100c1d2bf705c91a63e9ed85763b2bf3
Joe McColl <joe4kool@gmail.com>
2018-11-23 22:38:59 +0000

    ex3 test passing for adding to total cash

--- pet_shop.rb
+++ pet_shop.rb
@@ -3,5 +3,9 @@ def pet_shop_name(name)
 end
 
 def total_cash(sum)
-sum[:admin][:total_cash]
+  sum[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop_array,cash)
+  pet_shop_array[:admin][:total_cash] += cash
 end
