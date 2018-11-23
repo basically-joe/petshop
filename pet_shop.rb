86639504e77a88c033846acb1928e12fe271b159
Joe McColl <joe4kool@gmail.com>
2018-11-23 22:42:11 +0000

    ex4 solved by 3rd function and test passing in IDE

--- pet_shop.rb
+++ pet_shop.rb
@@ -6,6 +6,6 @@ def total_cash(sum)
   sum[:admin][:total_cash]
 end
 
-def add_or_remove_cash(pet_shop_array,cash)
-  pet_shop_array[:admin][:total_cash] += cash
+def add_or_remove_cash(pet_shop_hash,cash)
+  pet_shop_hash[:admin][:total_cash] += cash
 end
