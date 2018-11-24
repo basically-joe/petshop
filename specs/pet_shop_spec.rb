963d6aa46c6ff8110e35109c083ace28b7678d91
Joe McColl <joe4kool@gmail.com>
2018-11-24 00:06:07 +0000

    initial test fail for ex13

--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -135,12 +135,12 @@ class TestPetShop < Minitest::Test
   #   pet = find_pet_by_name(@pet_shop,"Arthur")
   #   assert_nil(pet)
   # end
-
-  # def test_add_pet_to_stock
-  #   add_pet_to_stock(@pet_shop, @new_pet)
-  #   count = stock_count(@pet_shop)
-  #   assert_equal(7, count)
-  # end
+#ex13
+  def test_add_pet_to_stock
+    add_pet_to_stock(@pet_shop, @new_pet)
+    count = stock_count(@pet_shop)
+    assert_equal(7, count)
+  end
 
   # def test_customer_cash
   #   cash = customer_cash(@customers[0])
