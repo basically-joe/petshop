5c7c1292b628cb81c49655900cdc6cb752f127ec
Joe McColl <joe4kool@gmail.com>
2018-11-24 00:01:55 +0000

    commenting out ex8, more research needed

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,23 +1,27 @@
 def pet_shop_name(name)
-  name[:name]
+  return name[:name]
 end
 
 def total_cash(sum)
-  sum[:admin][:total_cash]
+  return sum[:admin][:total_cash]
 end
 
 def add_or_remove_cash(pet_shop_hash,cash)
-  pet_shop_hash[:admin][:total_cash] += cash
+  return pet_shop_hash[:admin][:total_cash] += cash
 end
 
 def pets_sold(sold)
-sold[:admin][:pets_sold]
+return sold[:admin][:pets_sold]
 end
 
 def increase_pets_sold(pet_shop_hash,sold)
-  pet_shop_hash[:admin][:pets_sold] += sold
+  return pet_shop_hash[:admin][:pets_sold] += sold
 end
 
 def stock_count(count)
-count[:pets].count
+return count[:pets].count
 end
+
+# def pets_by_breed(pet_shop_hash, pets)
+#
+# end
